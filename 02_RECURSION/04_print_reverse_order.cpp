44708bc92a9c2f26f1342112ed19994280531f8d
Salih Edneer <98180446+salihednr@users.noreply.github.com>
2022-12-13 22:18:44 +0530

    Create 04_print_reverse_order.cpp

---
+++ 02_RECURSION/04_print_reverse_order.cpp
@@ -0,0 +1,22 @@
+/*
+EASY PROBLEM
+PRINTINT REVERSE ORDER INTEGER NUMBERS
+5
+5 4 3 2 1
+*/
+#include<iostream>
+using namespace std;
+void print(int n)
+{
+    if(n==0)
+        return;
+    cout<<n<<" ";
+    print(n-1);
+}
+int main()
+{
+    int n;
+    cin>>n;
+    print(n); 
+    return 0;
+}
