47515079ea2b321f4173a2f20127931d40fdeb2b
Salih Edneer <98180446+salihednr@users.noreply.github.com>
2022-12-25 20:44:17 +0530

    Update 16_first_position.cpp

--- 02_RECURSION/16_first_position.cpp
+++ 02_RECURSION/16_first_position.cpp
@@ -21,11 +21,21 @@ int findIndex2(int ar[],int n,int key,int i)
         return i;
     return findIndex2(ar,n,key,i+1);
 }
+//Method -3 Right To Left
+int findIndex3(int ar[],int n,int key,int i){
+    if(n==i)
+        return -1;
+    int t=findIndex3(ar,n,key,i+1);
+    if(ar[i]==key)
+        return i;
+    return t;
+}
 int main()
 {
     int ar[]={1,2,4,3,5,2,6,3,1};
     int n=sizeof(ar)/sizeof(int);
-    cout<<n-findIndex(ar,n,1)<<endl;
-    cout<<findIndex2(ar,n,1,0)<<endl;
+    cout<<n-findIndex(ar,n,2)<<endl;
+    cout<<findIndex2(ar,n,2,0)<<endl;
+    cout<<findIndex3(ar,n,2,0)<<endl;
     return 0;
 }
