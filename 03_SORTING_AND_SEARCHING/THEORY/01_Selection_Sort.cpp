eba49310988b6c1a21dcedc13e074ba2d4eaab7c
Salih Edneer <98180446+salihednr@users.noreply.github.com>
2022-12-26 21:57:46 +0530

    Create 01_Selection_Sort.cpp

---
+++ 03_SORTING_AND_SEARCHING/THEORY/01_Selection_Sort.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+using namespace std;
+void selectionSort(int ar[],int n){
+    for(int i=0;i<n-2;i++){
+        int min=i;
+        for(int j=i+1;j<n;j++)
+            if(ar[min]>ar[j])
+                min=j;
+        if(min!=i)
+            swap(ar[i],ar[min]);
+    }
+}
+int main()
+{
+    int n;
+    cin>>n;
+    int ar[n]={0};
+    //Input Array
+    for(int i=0;i<n;i++)
+        cin>>ar[i];
+    //Print Array
+    cout<<"Unsorted Array:"<<endl;
+    for(int i=0;i<n;i++)
+        cout<<ar[i]<<" ";
+    cout<<endl;
+    //Calling Selection Sorting Function
+    selectionSort(ar,n);
+    cout<<"Sorted Array:"<<endl;
+    for(int i=0;i<n;i++)
+        cout<<ar[i]<<" ";
+    cout<<endl;
+    return 0;
+}
