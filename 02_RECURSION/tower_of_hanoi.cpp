04425d4b89b771c53fcc4a6926b6464a40404675
Salih Edneer <98180446+salihednr@users.noreply.github.com>
2022-12-26 14:08:55 +0530

    Create tower_of_hanoi.cpp

---
+++ 02_RECURSION/tower_of_hanoi.cpp
@@ -0,0 +1,8 @@
+/*
+Problem Statement
+Consisting of three rods and various disk arranged in first rod move this disk into 3rd rod by obeying following rules
+1) Only one disk may be moved at a time
+2) No disk may be placed on top of a disk that is smaller than it
+Find Minimum Number Of Steps Required 
+Note: Optimal Solution For Minimum Number Of Steps Is 2^n-1
+*/
