278786aab00ee837683e4d8cd7b2d454317dd8d5
Salih Edneer <98180446+salihednr@users.noreply.github.com>
2022-12-29 13:48:31 +0530

    Create 2.3_Bubble_Sort.cpp

---
+++ 03_SORTING_AND_SEARCHING/THEORY/2.3_Bubble_Sort.cpp
@@ -0,0 +1,36 @@
+/*
+Bubble Sort
+Pushing Largest Element To The End Of The Array
+
+Time Complexity:
+Best Case: O(n^2)
+Average Case:O(n^2)
+Worst Case:O(n^2)
+Space Complexity:
+Constant Space, Space Is Not Increasing W.R.T to input
+O(1)
+*/
+#include <iostream>
+using namespace std;
+void bubbleSort(int a[],int n)
+{
+    for(int i=1;i<=n-1;i++){
+        for(int j=0;j<=n-2;j++){
+            if(a[j]>a[j+1])
+                swap(a[j],a[j+1]);
+        }
+    }
+}
+int main()
+{
+    int ar[]={1,7,10,2,5,8};
+    int n=sizeof(ar)/sizeof(int);
+    cout<<"UNSORTED:"<<endl;
+    for(int i=0;i<n;i++)
+        cout<<ar[i]<<" ";
+    bubbleSort(ar,n);
+    cout<<"\nSORTED"<<endl;
+    for(int i=0;i<n;i++)
+        cout<<ar[i]<<" ";
+    return 0;
+}
