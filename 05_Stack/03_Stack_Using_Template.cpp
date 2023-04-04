98720bd08584a2d21a628fd921fbe92876cec371
Salih Edneer <98180446+salihednr@users.noreply.github.com>
2023-04-05 00:08:42 +0530

    Update 03_Stack_Using_Template.cpp

--- 05_Stack/03_Stack_Using_Template.cpp
+++ 05_Stack/03_Stack_Using_Template.cpp
@@ -1,87 +1,111 @@
+/*
+Stacks Follow LIFO
+Stack Is An Abstract Data Type
+Operations
+    1.PUSH
+    2.POP
+    3.PRINT
+    4.ISEMPTY
+    5.SIZE
+    6.CAPACITY
+    7.TOP
+*/
 #include<iostream>
 #include<iomanip>
 using namespace std;
-template <class T>
+template<class T>
 class Stack{
     private:
-    T *ar;
-    int capacity;
-    int nextIndex;
+        T *arr;
+        int capacity;
+        int nextInd;
     public:
-    //size 
     //Default Constructor
     Stack(){
+        arr = new T[4];
         capacity=4;
-        ar=new T[capacity];
-        nextIndex=0;
+        nextInd=0;
     }
-    //Paramaterized Constructor
+    
+    //Parametrized Constructor
     Stack(int capacity){
-        ar=new T[capacity];
+        arr = new T[capacity];
         this->capacity=capacity;
-        nextIndex=0;
+        nextInd++;
     }
-    //Check Empty
+    //Check Stack Is Empty Or Not
     bool isEmpty(){
-        return nextIndex==0;
-    };
-    //Check Size
-    int sizeReturn(){
-        return nextIndex;
+        return nextInd==0;
     }
-    //Check Capacity
-    int capacityReturn(){
-        return capacity;
-    }
-    //pop
-    void pop(){
-        nextIndex--;
-    }
-    //push
-    void push(T data){
-        if(nextIndex<capacity)
-        {
-            ar[nextIndex]=data;
-            nextIndex++;
+    //Push Element
+    void push(T ele){
+        if(nextInd<capacity){
+            arr[nextInd]=ele;
+            nextInd++;
         }
         else{
-            cout<<"Size Exceed So Capacity Doubled"<<endl;
-            //Creating Temporary Dynamic Array
-            T *newArr=new T[2*capacity];
-            //Copying Elements
+            T *newArr= new T[2*capacity];
             for(int i=0;i<capacity;i++)
-                newArr[i]=ar[i];
-            //Deleteing Old Dynamically Created Array To Avoid Memory Leak
-            delete []ar;
-            ar=newArr;
+                newArr[i]=arr[i];
             capacity*=2;
-            ar[nextIndex]=data;
-            nextIndex++;
-            
+            delete []arr;
+            arr=newArr;
+            arr[nextInd]=ele;
+            nextInd++;
         }
     }
+    //Remove Element
+    void pop(){
+        if(isEmpty()){
+            cout<<"Underflow"<<endl;
+            return;
+        }
+        nextInd--;
+    }
+    //Print Last Element
+    int top(){
+        return arr[nextInd-1];
+    }
+
+    //Print Stack
     void print(){
-        for(int i=0;i<nextIndex;i++)
-            cout<<"|"<<setw(5)<<left<<ar[nextIndex-i-1]<<"|"<<endl;
+        for(int i=0;i<nextInd;i++)
+            cout<<"|"<<setw(5)<<left<<arr[nextInd-i-1]<<"|"<<endl;
         cout<<"|-----|"<<"\n\n";
     }
+    //Size Of The Stack
+    int size(){
+        return nextInd;
+    }
+    //Capacity Of The Stack
+    int Capacity(){
+        return capacity;
+    }
+    //Destructor
     ~Stack(){
-        cout<<"Destructor Is Called"<<endl;
-        delete []ar;
+        delete []arr;
     }
 };
+
 int main(){
-    cout<<"Welcome To Stack Operations"<<endl;
-    Stack<char>s1(5);
-    s1.push(105);
-    s1.push(106);
-    s1.push(107);
-    s1.push(108);
-    s1.push(109);
-    s1.push(110);
-    s1.push(111);
+    Stack<char> s1;
+    s1.push(95);
+    s1.push(96);
+    s1.push(97);
+    cout<<"Size Of The Stack: "<<s1.size()<<endl;
+    cout<<"Capacity Of The Stack: "<<s1.Capacity()<<endl;
     s1.print();
     s1.pop();
-    s1.print();
+    s1.push(22);
+    s1.push(32);
+    s1.push(33);
+    cout<<"Size Of The Stack: "<<s1.size()<<endl;
+    cout<<"Capacity Of The Stack: "<<s1.Capacity()<<endl;
+    s1.pop();
+    s1.pop();
+    s1.pop();
+    s1.pop();
+    s1.pop();
+    s1.pop();
     return 0;
 }
